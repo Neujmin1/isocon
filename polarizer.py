bc0997a332843d4a4ea0bf79c918ffc47223a3af
Neujmin1 <Neujmin1>
2018-06-07 10:17:27 -0600

    added general jones and mueller representations/methods

--- polarizer.py
+++ polarizer.py
@@ -19,7 +19,35 @@ class Polarizer(OpticalElement):
 
         # contrast ratio?
 
-        #
+    def general_mueller(self, rotation_degrees_ccw):
+        # general formula of linear polarizer of given rotation
+
+        rads = rotation_degrees_ccw*numpy.pi/180
+
+        c2 = numpy.cos(2*rads)
+        s2 = numpy.sin(2*rads)
+
+        r1 = numpy.array([1, c2, s2, 0])
+        r2 = numpy.array([c2, c2*c2, c2*s2, 0])
+        r3 = numpy.array([s2, c2*s2, s2*s2, 0])
+        r4 = numpy.array([0, 0, 0, 0])
+
+        return 0.5*numpy.matrix(numpy.array([r1, r2, r3, r4]))
+
+    def general_jones(self, rotation_degrees_ccw):
+
+        rads = rotation_degrees_ccw * numpy.pi / 180
+
+        c1 = numpy.cos(2*rads)
+        s1 = numpy.sin(2*rads)
+
+        r1 = numpy.array([c1*c1, c1*s1])
+        r2 = numpy.array([c1*s1, s1*s1])
+
+        return numpy.matrix(numpy.array([r1, r2]))
+
+
+
 
 
 
