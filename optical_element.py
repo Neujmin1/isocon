669592eac35611a429403cc70e601c270cd9c5f6
Neujmin1 <Neujmin1@gmail.com>
2018-06-07 00:50:01 -0600

    added relative and absolute rotation to class

--- optical_element.py
+++ optical_element.py
@@ -1,15 +1,33 @@
+import mathops
+import numpy
+
 
 class OpticalElement:
 
     def __init__(self):
 
-        # in general, these 3 could be a lookup table via empical lambda-dependence
+        # in general, these 3 could be a lookup table via empirical lambda-dependence
         self.transmission = 1
         self.reflection = 0
         self.absorbtion = 0
-
-        self.rotation_degrees = 0  # measured ccw from x-axis. Light along z-axis
+        self.degrees_rotated_ccw = 0  # measured ccw from x-axis. Light along z-axis
+        self.attitude = numpy.eye(3)  # orientation in 3D (reserved for advanced operations)
 
         # dispersion relation?
 
-        # how to handle AR coating?
+        # AR coating?
+
+        self.jones = None
+        self.mueller = None
+
+    def relative_rotate(self, degrees_ccw=45):
+
+        self.mueller = mathops.rotate_mueller(self.mueller, degrees_ccw)
+        self.jones = mathops.rotate_jones(self.jones, degrees_ccw)
+        self.degrees_rotated_ccw += degrees_ccw
+
+    def absolute_rotate(self, degrees_ccw=0):
+
+        relative_degrees_to_rotate = degrees_ccw - self.degrees_rotated_ccw
+        self.relative_rotate(relative_degrees_to_rotate)
+        self.degrees_rotated_ccw = degrees_ccw
