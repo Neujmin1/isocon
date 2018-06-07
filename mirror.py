86c3cd511a66d9a6c420c26625f85b84f4ee7634
Neujmin1 <Neujmin1@gmail.com>
2018-06-07 00:50:28 -0600

    added mueller matrix. it doesn't seem to give correct results so i'll need to check it further

--- mirror.py
+++ mirror.py
@@ -1,7 +1,13 @@
 from optical_element import OpticalElement
+import numpy
 
 
 class Mirror(OpticalElement):  # just changes phase by pi
 
     def __init__(self):
         OpticalElement.__init__(self)
+
+        self.mueller = numpy.eye(4)
+        self.mueller[2:, 2:] *= -1
+
+        self.jones = numpy.array([[1, 0], [0, -1]])
