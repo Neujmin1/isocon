026868911c6e8fa3e9e367ed273afc5e2214080e
Neujmin1 <Neujmin1>
2018-06-21 13:36:29 -0600

    compensator test

--- compensator_model.py
+++ compensator_model.py
@@ -1,10 +1,14 @@
 
 from __future__ import division
 
+import matplotlib.pyplot as plt
+
+import numpy
 from mirror import Mirror
 from polarizer import Polarizer
 from waveplate import WavePlate
 from incoherent import Incoherent
+from compound_element import CompoundElement
 
 if __name__ == '__main__':
 
@@ -15,6 +19,30 @@ if __name__ == '__main__':
     vertical_polarizer = Polarizer()
     vertical_polarizer.absolute_rotate(90)
 
-    tca_18 = WavePlate(degrees_retardance=360*(18/560))
-    tca_35 = WavePlate(degrees_retardance=360*(35/360))
+    mirror1 = Mirror()
+
+    compensator = WavePlate(degrees_retardance=360 * (18 / 560))
+
+    # always present order for light wave coming from the right
+    compound_item = CompoundElement([mirror1, compensator])  # this object will be rotated
+
+    # make incoherent, linearly polarized light. Horizontal/along x-axis
+    light = Incoherent()
+    linearly_polarized_light = numpy.matmul(horizontal_polarizer.mueller, light.stokes_vector)
+
+    # just normalizing relative to newly-polarized beam's intensity
+    linearly_polarized_light /= linearly_polarized_light[0]
+
+    angles = []
+    intensities = []
+    for degrees in numpy.linspace(0, 360, 500):
+        compound_item.absolute_rotate(degrees)
+        M = numpy.matmul(compound_item.mueller, horizontal_polarizer.mueller)
+        M = numpy.matmul(vertical_polarizer.mueller, M)
+        stokes = numpy.matmul(M, linearly_polarized_light)
+        intensities.append(stokes[0, 0].real)
+        angles.append(degrees)
+
+    plt.plot(angles, intensities)
+    plt.show()
 
